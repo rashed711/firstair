5f89421ccab4f61af2e2d2e4ad55cc80fdc59449
Rashed <rashed1711@gmail.com>
2025-02-18 16:25:32 +0200

    3 commit

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ include "./php_fun/login_fun.php"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>First Air | Login 1</title>
+    <title>First Air | Login</title>
     <link rel="stylesheet" href="css/style.css">
 
 </head>
