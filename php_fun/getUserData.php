f3d925f8660067e7394e623c553d67bf30e8f8c5
Rashed <rashed1711@gmail.com>
2025-08-10 11:34:20 +0300

    first air Submittal page

--- php_fun/getUserData.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(error_level: 0);
-session_start();
-$stm = "select * from users where email='$_SESSION[email]'";
-$result = mysqli_query($conn,  $stm);
-$row = mysqli_fetch_array($result);
-
-?>
